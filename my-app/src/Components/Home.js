5004c067b4b914451dfcae37aeddc5c58dc58f14
Neeraj Kumar <neeraj.dsu@gmail.com>
2023-11-07 00:01:27 +0500

    Updated home

--- my-app/src/Components/Home.js
+++ my-app/src/Components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 export default function Home() {
 
     return (
-        <div className='m-4' >
+        <div className='m-5' >
             <h1 className="text-center">Welcome to EdVenture</h1>
             <h3>Bank of the free</h3>
             <p>Lorem ipsum dolor sit amet...</p>
